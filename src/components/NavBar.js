b2d8371fc25218e285a247ba1952b3e5156d47b4
hitochix06 <pachara.philibert@hotmail.fr>
2024-02-20 15:06:02 +0100

    Update the homepage link in the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ export default function Example() {
           aria-label="Global"
         >
           <div className="flex lg:flex-1 items-center">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <img
                 className="h-20 w-auto"
                 src="logo.jpg"
