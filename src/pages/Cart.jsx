47e0d13e37726aa2ba109a8a68aeed11eb358188
hitochix06 <pachara.philibert@hotmail.fr>
2024-02-21 16:45:04 +0100

    Refactor Cart and Reservation components

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,61 @@
-import { useState } from "react";
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <main className="isolate">
+        {/* Image section */}
+        <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
+          <div className="text-center py-8">
+            <div className="mt-6">
+              <div className="max-w-4xl mx-auto">
+                {/* Info, occupe 2 fractions */}
+                <div className="md:col-span-2 bg-white shadow-lg rounded-lg p-8">
+                  {" "}
+                  {/* Ajustez pour que cette div occupe 2/3 de l'espace disponible */}
+                  <h2 className="text-3xl font-bold text-gray-800">
+                    Votre Panier
+                  </h2>
+                  {/* Contenu de la deuxième colonne ici */}
+                  <div className="flex flex-col lg:flex-row  justify-between items-center shadow-lg p-4 ">
+                    <div className="flex flex-col sm:flex-row sm:flex-grow sm:items-center space-x-0 sm:space-x-10 space-y-2 sm:space-y-0 ">
+                      {/* Ajustement pour l'alignement et l'espacement */}
+                      <h4 className="text-xl font-semibold flex-shrink-0">
+                        Départ: Paris - Arrivée: Nice
+                      </h4>
+                      <p className="text-lg flex-shrink-0">Date: 2023-05-20</p>
+                      <p className="text-lg flex-shrink-0">Prix: 85€</p>
+                    </div>
+                    <button className="mt-5 sm:mt-0 sm:ml-4 inline-flex items-center justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 mr-2"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                        />
+                      </svg>
+                      Supprimer
+                    </button>
+                  </div>
+                </div>
 
-export default function Cart() {
-  return <div className="all">TOTOS</div>;
+                <div className="text-center py-8">
+                  <div className="mt-6">
+                    <div className="max-w-4xl mx-auto">
+                      <div className="md:col-span-2 bg-white shadow-lg rounded-lg p-8"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/pages/Reservation.jsx
+++ src/pages/Reservation.jsx
@@ -1,12 +1,5 @@
 
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
-
-
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
     <div className="bg-white">
