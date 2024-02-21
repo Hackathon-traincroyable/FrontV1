50499a9b9dcc1d89d707f47fb7862ae207997212
SoulSkunk <kylian.broccolichi@mediaschool.me>
2024-02-21 11:38:39 +0100

    Footer :tada

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './style/globale.css';
+import "./style/globale.css";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import LogSign from "./pages/LogSign";
 import Reservation from "./pages/Reservation";
 import NavBar from "./components/NavBar";
-
+import Footer from "./components/Footer";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -19,8 +19,8 @@ function App() {
           <Route path="/reservation" element={<Reservation />} />
           <Route path="/logsign" element={<LogSign />} />
         </Routes>
+        <Footer />
       </Router>
-
     </>
   );
 }
